5a6e3604abba2443c409a8bc5aaa34e0b77425e9
Zammad Nasir <zammadnasir@gmail.com>
2026-06-12 18:40:34 +0500

    feat: Arena legends

---
+++ games/arena-legends/meta.ts
@@ -0,0 +1,16 @@
+import { GameMeta } from "@/lib/game-types";
+import thumbnail from "./thumbnail.png";
+
+export const meta: GameMeta = {
+  slug: "arena-legends",
+  name: "Arena Legends",
+  description:
+    "A fast-paced 1v1 fighting game with 6 unique fighters, combo system, and arcade mode. Defeat all opponents to become the ultimate arena champion!",
+  genre: "Action",
+  layout: "immersive",
+  thumbnail,
+  generatedWith: "MiniMax-M2.7",
+  status: "Playable",
+  prompts: [],
+  knownIssues: [],
+};
